db32c93895c272ede294f51d4617c6df54f3957f
eduardovale76 <eduardovale76@gmail.com>
2020-07-11 18:40:29 -0300

    Atributo Complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade =35):
+    def __init__(self, *filhos, nome=None, idade =35):
+        self.filhos = list(filhos)
         self.idade = idade
         self.nome = nome
 
     def cumprimentar(self):
-        return f'ola {id(p)}'
+        return f'ola {id(eduardo)}'
 
 if __name__ == '__main__':
-    p = Pessoa("teste")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Eduardo'
-    print(p.nome, p.idade)
+    rafael = Pessoa(nome='Rafael', idade=7)
+    eduardo = Pessoa(rafael, nome='Eduardo', idade=44)
+    print(Pessoa.cumprimentar(eduardo))
+    print(rafael.cumprimentar())
+    print(rafael.nome, rafael.idade)
+    print(eduardo.nome, eduardo.idade)
+    for filho in eduardo.filhos:
+        print(f'{filho.nome} é filho de {eduardo.nome}')
