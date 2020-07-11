2a97ceb00ff5a1dde30f29b464d37f32d5acc663
eduardovale76 <eduardovale76@gmail.com>
2020-07-11 17:50:27 -0300

    Criada Classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,2 @@
 class Pessoa:
-    def cumprimentar(self):
-        return f'Ola {id(self)}'
-
-if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-
+    pass
