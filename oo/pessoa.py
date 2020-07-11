a5505fb2f966b32d94c2a1a30e3feecfdc7b3efd
eduardovale76 <eduardovale76@gmail.com>
2020-07-11 18:27:49 -0300

    Criando atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade =35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'ola {id(p)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("teste")
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Eduardo'
+    print(p.nome, p.idade)
